71b60316ae501ee313baaf50473c322ce20939e4
John Rodger <john.rodger@atheneum.ai>
2026-01-29 21:25:28 +0000

    add manual test for embedding create

--- manual_test_embedder.py
+++ manual_test_embedder.py
@@ -2,9 +2,11 @@ import json
 from src.embedder import create_embeddings
 
 
-with open("storage/ai_overview_chunks.json", "r") as file:
+with open("storage/climate_change_chunks.json", "r") as file:
   chunks = json.load(file)
 
-  batches = create_embeddings(chunks)
+  embeddings = create_embeddings(chunks)
+  embeddings_list = embeddings.tolist()
 
-  print(batches)
+  with open("storage/climate_change_embeddings.json", "w", encoding='utf-8') as f:
+    json.dump(embeddings_list, f)
